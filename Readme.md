9a26223f97b39cead328ba6e75b96463ad1878d0
Jake Rosoman <jkroso@gmail.com>
2013-12-30 14:10:33 +1300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,22 @@
+
+# hydro-html
+
+  A [hydro](//github.com/hydrojs/hydro) formatter which renders to the DOM
+
+## Installation
+
+With your favourite package manager:
+
+- [packin](//github.com/jkroso/packin): `packin add hydro-html`
+- [component](//github.com/component/component#installing-packages): `component install hydrojs/hydro-html`
+- [npm](//npmjs.org/doc/cli/npm-install.html): `npm install hydro-html`
+
+Then set it as your formatter:
+
+```js
+hydro.set('formatter', require('hydro-html'))
+```
+
+## Thanks
+
+  Based on the [mocha](//github.com/visionmedia/mocha) html reporter
---
+++ index.js
@@ -0,0 +1,302 @@
+
+var bind = require('event-component').bind
+var Progress = require('progress-svg')
+var escape = require('escape-html')
+var classes = require('classes')
+var domify = require('domify')
+var text = require('text')
+
+/**
+ * Save timer references because some testing tools
+ * mess with them
+ */
+
+var global = function(){return this}()
+var Date = global.Date
+var setTimeout = global.setTimeout
+var setInterval = global.setInterval
+var clearTimeout = global.clearTimeout
+var clearInterval = global.clearInterval
+
+/**
+ * Expose `HTML`.
+ */
+
+exports = module.exports = HTML
+
+/**
+ * Stats template.
+ */
+
+var statsTemplate = '<ul id="hydro-stats">'
+  + '<li class="progress"></li>'
+  + '<li class="passes"><a href="#">passes:</a> <em>0</em></li>'
+  + '<li class="failures"><a href="#">failures:</a> <em>0</em></li>'
+  + '<li class="duration">duration: <em>0</em>s</li>'
+  + '</ul>'
+
+/**
+ * Initialize a new `HTML` reporter.
+ *
+ * @param {Hydro} hydro
+ * @param {Element} root
+ * @api public
+ */
+
+function HTML(hydro, root){}
+
+HTML.prototype.use = function(hydro, root){
+  var self = this
+  var stats = track(hydro)
+  var stat = domify(statsTemplate)
+  var items = stat.getElementsByTagName('li')
+  var passes = items[1].getElementsByTagName('em')[0]
+  var passesLink = items[1].getElementsByTagName('a')[0]
+  var failures = items[2].getElementsByTagName('em')[0]
+  var failuresLink = items[2].getElementsByTagName('a')[0]
+  var duration = items[3].getElementsByTagName('em')[0]
+  var progress = new Progress().size(40)
+  var report = domify('<ul id="hydro-report"></ul>')
+  var stack = [report]
+
+  root = root || document.getElementById('hydro')
+  if (!root) return error('#hydro div missing, add it to your document')
+
+  items[0].appendChild(progress.el)
+  root.appendChild(stat)
+  root.appendChild(report)
+
+  // pass toggle
+  bind(passesLink, 'click', function(){
+    unhide()
+    var name = /pass/.test(report.className) ? '' : ' pass'
+    report.className = report.className.replace(/fail|pass/g, '') + name
+    if (report.className.trim()) hideSuitesWithout('test pass')
+  })
+
+  // failure toggle
+  bind(failuresLink, 'click', function(){
+    unhide()
+    var name = /fail/.test(report.className) ? '' : ' fail'
+    report.className = report.className.replace(/fail|pass/g, '') + name
+    if (report.className.trim()) hideSuitesWithout('test fail')
+  })
+
+  hydro.on('pre:suite', function(suite){
+
+    // suite
+    var url = self.suiteURL(suite)
+    var el = fragment('<li class="suite"><h1><a href="%s">%e</a></h1></li>', url, suite.title)
+
+    // container
+    stack[0].appendChild(el)
+    stack.unshift(document.createElement('ul'))
+    el.appendChild(stack[0])
+  })
+
+  hydro.on('post:suite', function(suite){
+    if (suite.root) return
+    stack.shift()
+  })
+
+  hydro.on('fail', function(test, err){
+    if ('hook' == test.type) hydro.emit('test end', test)
+  })
+
+  hydro.on('pre:test', function(test){
+    test.start = new Date
+  })
+
+  hydro.on('post:test', function(test){
+    progress.update(stats.percent)
+    test.duration = new Date - test.start
+    test.speed = 'fast'
+
+    // update stats
+    text(passes, stats.passes)
+    text(failures, stats.failures)
+    text(duration, (stats.elapsed / 1000).toFixed(2))
+
+    // test
+    switch (test.status) {
+      case 'passed':
+        var el = fragment(
+          '<li class="test pass %s"><h2>%e<span class="duration">%ems</span> <a href="%s" class="replay">‣</a></h2></li>',
+          test.speed,
+          test.title,
+          test.duration,
+          self.testURL(test))
+        break
+      case 'pending':
+      case 'skipped':
+        var el = fragment('<li class="test pass pending"><h2>%e</h2></li>', test.title)
+        break
+      case 'failed':
+        var el = fragment('<li class="test fail"><h2>%e <a href="?grep=%e" class="replay">‣</a></h2></li>', test.title, encodeURIComponent(test.title))
+        var str = test.err.stack || test.err.toString()
+
+        // FF / Opera do not add the message
+        if (!~str.indexOf(test.err.message)) {
+          str = test.err.message + '\n' + str
+        }
+
+        // <=IE7 stringifies to [Object Error]. Since it can be overloaded, we
+        // check for the result of the stringifying.
+        if ('[object Error]' == str) str = test.err.message
+
+        // Safari doesn't give you a stack. Let's at least provide a source line.
+        if (!test.err.stack && test.err.sourceURL && test.err.line !== undefined) {
+          str += "\n(" + test.err.sourceURL + ":" + test.err.line + ")"
+        }
+
+        el.appendChild(fragment('<pre class="error">%e</pre>', str))
+    }
+
+    // toggle code
+    // TODO: defer
+    if (test.status != 'pending') {
+      var h2 = el.getElementsByTagName('h2')[0]
+      bind(h2, 'click', function(){
+        pre.style.display = 'none' == pre.style.display
+          ? 'block'
+          : 'none'
+      })
+      var pre = fragment('<pre><code>%e</code></pre>', clean(test.fn.toString()))
+      el.appendChild(pre)
+      pre.style.display = 'none'
+    }
+
+    // Don't call .appendChild if #hydro-report was already
+    // .shift()'ed off the stack.
+    if (stack[0]) stack[0].appendChild(el)
+  })
+}
+
+function track(hydro){
+  var stats = {
+    pending: 0,
+    tests: 0,
+    passes: 0,
+    failures: 0,
+    total: hydro.tests().length
+  }
+
+  hydro.on('post:test', function(test){
+    stats.percent = (++stats.tests / stats.tests) * 100 | 0
+    stats.elapsed = new Date - stats.start
+    switch (test.status) {
+      case 'pending':
+      case 'skipped':
+        stats.pending++
+        break
+      case 'passed':
+        stats.passes++
+        break
+      case 'failed':
+        stats.failures++
+    }
+  })
+
+  hydro.on('pre:all', function() {
+    stats.start = new Date
+    stats.tests = hydro.tests().length
+  })
+
+  hydro.on('post:all', function(){
+    stats.end = new Date
+    stats.duration = Number(stats.end) - Number(stats.start)
+  })
+
+  return stats
+}
+
+/**
+ * Provide suite URL
+ *
+ * @param {Object} [suite]
+ */
+
+HTML.prototype.suiteURL = function(suite){
+  // TODO: fullTitle
+  return '?grep=' + encodeURIComponent(suite.title)
+}
+
+/**
+ * Provide test URL
+ *
+ * @param {Object} [test]
+ */
+
+HTML.prototype.testURL = function(test){
+  // TODO: fullTitle
+  return '?grep=' + encodeURIComponent(test.title)
+}
+
+/**
+ * Display error `msg`.
+ */
+
+function error(msg) {
+  document.body.appendChild(fragment('<div id="hydro-error">%s</div>', msg))
+}
+
+/**
+ * Return a DOM fragment from `html`.
+ */
+
+function fragment(html) {
+  var args = arguments
+  var i = 1
+  return domify(html.replace(/%([se])/g, function(_, type){
+    switch (type) {
+      case 's': return String(args[i++])
+      case 'e': return escape(args[i++])
+    }
+  }))
+}
+
+/**
+ * Check for suites that do not have elements
+ * with `classname`, and hide them.
+ */
+
+function hideSuitesWithout(classname) {
+  var suites = document.getElementsByClassName('suite')
+  for (var i = 0; i < suites.length; i++) {
+    var suite = suites[i]
+    var els = suite.getElementsByClassName(classname)
+    if (!els.length) classes.add('hidden', suite)
+  }
+}
+
+/**
+ * Unhide .hidden suites.
+ */
+
+function unhide() {
+  var els = document.getElementsByClassName('suite hidden')
+  for (var i = 0; i < els.length; ++i) classes.remove('hidden', els[i])
+}
+
+/**
+ * Strip the function definition from `str`
+ *
+ * @param {String} str
+ * @return {String}
+ * @api private
+ */
+
+function clean(str) {
+  str = str
+    .replace(/\r\n?|[\n\u2028\u2029]/g, "\n").replace(/^\uFEFF/, '')
+    .replace(/^function *\(.*\) *{/, '')
+    .replace(/\s+\}$/, '')
+
+  var spaces = str.match(/^\n?( *)/)[1].length
+  var tabs = str.match(/^\n?(\t*)/)[1].length
+  var re = new RegExp(''
+    + '^\n?' + (tabs ? '\t' : ' ')
+    + '{' + (tabs || spaces) + '}', 'gm')
+
+  return str.replace(re, '').trim()
+}
